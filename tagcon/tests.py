dbde2761116401e8a0b0bf86a4b9ccd186e513cf
Rob Berry <rob@luvhate.us>
2009-12-05 19:38:16 +0000

    test for ModelInstanceArg

--- tagcon/tests.py
+++ tagcon/tests.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
 from django.template import Library, Template, Context, add_to_builtins,\
         TemplateSyntaxError, VariableDoesNotExist
+from django.db import models
 import tagcon
 register = Library()
+
+
+class Link(models.Model):
+
+    url = models.URLField()
+
+    def __unicode__(self):
+        return u'<%s>' % self.url
+
 
 class KeywordTag(tagcon.TemplateTag):
 
@@ -21,11 +31,16 @@ class NoArgumentTag(tagcon.TemplateTag):
 class ArgumentTypeTag(tagcon.TemplateTag):
 
     age = tagcon.IntegerArg()
-    handle = tagcon.StringArg()
+    name_ = tagcon.StringArg()
+    url = tagcon.ModelInstanceArg(model=Link, required=False,
+                                        null=True)
 
     def render(self, context):
         self.resolve(context)
-        return '%s is %d' % (self.args.handle, self.args.age)
+        str = '%s %d' % (self.args.name, self.args.age)
+        if self.args.url is not None:
+            str += ' %s' % self.args.url
+        return str
 
 add_to_builtins(KeywordTag.__module__)
 add_to_builtins(NoArgumentTag.__module__)
@@ -71,21 +86,26 @@ class TagCreationTests(TestCase):
 
         render = lambda t: Template(t).render(Context())
 
-        t = Template('{% argument_type age 101 handle "alice" %}')
-        self.assertEqual(t.render(Context()), 'alice is 101')
+        t = Template('{% argument_type age 101 name "alice" %}')
+        self.assertEqual(t.render(Context()), 'alice 101')
 
         # IntegerArg.clean calls int(value) to convert "101" to 101
-        t = Template('{% argument_type age "101" handle "alice" %}')
-        self.assertEqual(t.render(Context()), 'alice is 101')
+        t = Template('{% argument_type age "23" name "bob" %}')
+        self.assertEqual(t.render(Context()), 'bob 23')
 
         # IntegerArg.clean will choke on the string
         self.assertRaises(tagcon.TemplateTagValidationError,
                           render,
-                          '{% argument_type age "101b" handle "alice" %}')
+                          '{% argument_type age "7b" name "charlie" %}')
 
         # will not find a var named alice in the context
         try:
-            render('{% argument_type age "101" handle alice %}')
+            render('{% argument_type age "44" name dave %}')
         except TemplateSyntaxError, e:
             self.assertTrue(isinstance(e.exc_info[1], VariableDoesNotExist))
+
+        t = Template('{% argument_type age 19 name "eve" url object %}')
+        object = Link(url='http://bing.com')
+        c = Context({'object': object})
+        self.assertEqual(t.render(c), 'eve 19 <http://bing.com>')
 
