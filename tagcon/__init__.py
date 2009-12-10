9ec974666e4031abceab97a9aff62a804095b877
Tom X. Tobin <tomxtobin@tomxtobin.com>
2009-12-09 21:23:23 -0600

    check that we're Python 2.5+ (but not Python 3)

--- tagcon/__init__.py
+++ tagcon/__init__.py
@@ -1,1 +1,8 @@
+import sys
+if sys.version_info[0] == 3:
+    raise SystemExit("Django-Tagcon does not yet support Python 3")
+if sys.version_info[0] != 2 or sys.version_info[1] < 5:
+    raise SystemExit("Django-Tagcon requires Python 2.5 or greater")
+del sys
+
 from tagcon.base import *
