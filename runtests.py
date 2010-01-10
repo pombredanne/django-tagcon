feaab99c98096e9835c83d9766848ca9cf2a73b7
Carl Meyer <carl@dirtcircle.com>
2010-01-10 11:47:35 +0800

    provide minimal test settings so DJANGO_SETTINGS_MODULE is not required to run tests

--- runtests.py
+++ runtests.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import unittest
+import os
+
+if not 'DJANGO_SETTINGS_MODULE' in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 
 from django.conf import settings
 from django.db import connection
 from django.test.utils import setup_test_environment
 from django.test.utils import teardown_test_environment
 from django.utils.importlib import import_module
-
 
 TEST_MODULES = ["tests.base"]
 
