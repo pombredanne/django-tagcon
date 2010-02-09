ee1dd0dc9b0bc5229d639ff261e53c239b0bbbd4
Tom X. Tobin <tomxtobin@tomxtobin.com>
2010-02-09 10:38:24 -0600

    fix import for Python 2.5
    Python 2.5 breaks on "from .FOO import *", so we do the same thing in a
    roundabout manner.

--- tagcon/__init__.py
+++ tagcon/__init__.py
@@ -5,4 +5,8 @@ if sys.version_info[0] != 2 or sys.version_info[1] < 5:
     raise SystemExit("Django-Tagcon requires Python 2.5 or greater")
 del sys
 
-from .base import *
+# Python 2.5 doesn't support ``from .base import *``, so we do the same thing
+# in a roundabout manner.
+from .base import __all__ as _all
+globals().update(dict((x, getattr(base, x)) for x in _all))
+del _all
